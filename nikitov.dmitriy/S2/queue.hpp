4186f605c031839b84fd0530c07d5bb6021bc0ac
Dmitriy <Dmitry5335@yandex.ru>
2024-03-18 15:01:54 +0300

    Queue constructor

--- nikitov.dmitriy/S2/queue.hpp
+++ nikitov.dmitriy/S2/queue.hpp
@@ -1,14 +1,34 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
 
+#include <cstddef>
+#include "stack.hpp"
+
 namespace nikitov
 {
   template< class T >
   class Queue
   {
   public:
+    Queue();
+    ~Queue() = default;
+
     void push(const T& value);
     T& drop();
+
+  private:
+    Stack< T > data1_;
+    Stack< T > data2_;
+    size_t& size_;
+    size_t& capacity_;
   };
+
+  template< class T >
+  Queue< T >::Queue():
+    data1_(),
+    data2_(),
+    size_(data1_.size_),
+    capacity_(data1_.capacity_)
+  {}
 }
 #endif
