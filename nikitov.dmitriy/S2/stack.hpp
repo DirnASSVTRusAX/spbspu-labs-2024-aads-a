1b654765a876068990953fbf1735b23bdfb2a56b
Dmitriy <Dmitry5335@yandex.ru>
2024-03-18 14:40:52 +0300

    Stack push and resize

--- nikitov.dmitriy/S2/stack.hpp
+++ nikitov.dmitriy/S2/stack.hpp
@@ -1,6 +1,8 @@
 #ifndef STACK_HPP
 #define STACK_HPP
 
+#include <cstddef>
+
 namespace nikitov
 {
   template< class T >
@@ -13,14 +15,16 @@ namespace nikitov
     void push(const T& value);
     T& drop();
   private:
-    T** data_;
+    void resize();
+
+    T* data_;
     size_t size_;
     size_t capacity_;
   };
 
   template< class T >
   Stack< T >::Stack():
-    data_(new T*[20]),
+    data_(new T[20]),
     size_(0),
     capacity_(20)
   {}
@@ -29,6 +33,30 @@ namespace nikitov
   Stack< T >::~Stack()
   {
     delete[] data_;
+  }
+
+  template< class T >
+  void Stack< T >::push(const T& value)
+  {
+    if (size_ == capacity_)
+    {
+      resize();
+    }
+    data_[size_] = value;
+    ++size_;
+  }
+
+  template< class T >
+  void Stack< T >::resize()
+  {
+    capacity_ += 20;
+    T* newData = new T[capacity_];
+    for (size_t i = 0; i != size_; ++i)
+    {
+      newData[i] = data_[i];
+    }
+    delete[] data_;
+    data_ = newData;
   }
 }
 #endif
