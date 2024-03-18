9dbf05b90ceb32b8da9bff88c18c7c94f1762ffa
Dmitriy <Dmitry5335@yandex.ru>
2024-03-18 14:10:52 +0300

    Constructor, destructor in Stack

--- nikitov.dmitriy/S2/stack.hpp
+++ nikitov.dmitriy/S2/stack.hpp
@@ -7,8 +7,28 @@ namespace nikitov
   class Stack
   {
   public:
+    Stack();
+    ~Stack();
+
     void push(const T& value);
     T& drop();
+  private:
+    T** data_;
+    size_t size_;
+    size_t capacity_;
   };
+
+  template< class T >
+  Stack< T >::Stack():
+    data_(new T*[20]),
+    size_(0),
+    capacity_(20)
+  {}
+
+  template< class T >
+  Stack< T >::~Stack()
+  {
+    delete[] data_;
+  }
 }
 #endif
