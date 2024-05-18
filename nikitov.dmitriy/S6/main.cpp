902410cb9cffa0d87a960b5f0d88638b0c469d02
Dmitriy <Dmitry5335@yandex.ru>
2024-05-18 20:08:18 +0300

    Add parameters check and creating of structs

--- nikitov.dmitriy/S6/main.cpp
+++ nikitov.dmitriy/S6/main.cpp
@@ -1,2 +1,17 @@
-int main()
-{}
+#include <iostream>
+#include <forward_list>
+#include <deque>
+#include <list.hpp>
+
+int main(int argc, char* argv[])
+{
+  using namespace nikitov;
+  if (argc != 4)
+  {
+    std::cerr << "Error: Wrong input parameters" << '\n';
+    return 1;
+  }
+  std::forward_list< float > list;
+  List< float > biList;
+  std::deque< float > deque;
+}
