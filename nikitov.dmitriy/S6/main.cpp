77f6ec7e9f8a8208d6309794c032fe6a502d4ebd
Dmitriy <Dmitry5335@yandex.ru>
2024-05-19 14:10:48 +0300

    Make zero size check

--- nikitov.dmitriy/S6/main.cpp
+++ nikitov.dmitriy/S6/main.cpp
@@ -13,105 +13,22 @@ int main(int argc, char* argv[])
     return 1;
   }
 
-  size_t size = {};
-  try
-  {
-    size = std::stoull(argv[3]);
-  }
-  catch (const std::exception&)
+  size_t size = std::stoull(argv[3]);
+  if (size == 0)
   {
     std::cerr << "Error: Wrong size parameter" << '\n';
     return 1;
   }
 
-  std::string type = argv[2];
-
-  std::forward_list< int > firstList;
-
-  if (type == "ints")
+  List< int > list;
+  for (size_t i = 0; i != size; ++i)
   {
-    for (size_t i = 0; i != size; ++i)
-    {
-      float num = rand();
-      firstList.push_front(num);
-    }
+    list.push_back(std::rand());
   }
-  else if (type == "floats")
+  for (auto i = list.cbegin(); i != list.cend(); ++i)
   {
-    //for (size_t i = 0; i != size; ++i)
-    //{
-      //float num = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX));
-      //firstList.push_front(num);
-    //}
+    std::cout << *i;
   }
-  else
-  {
-    std::cerr << "Error: Wrong type parameter" << '\n';
-    return 1;
-  }
-
-  firstList.reverse();
-  std::forward_list< int > secondList(firstList);
-  List< int > firstBiList;
-  for (auto i = firstList.cbegin(); i != firstList.cend(); ++i)
-  {
-    firstBiList.push_back(*i);
-  }
-  List< int > secondBiList(firstBiList);
-  std::deque< int > firstDeque;
-  for (auto i = firstList.cbegin(); i != firstList.cend(); ++i)
-  {
-    firstDeque.push_back(*i);
-  }
-  std::deque< int > secondDeque(firstDeque);
-
-  for (auto i = firstList.cbegin(); i != firstList.cend(); ++i)
-  {
-    std::cout << (*i) << ' ';
-  }
-  std::cout << '\n';
-
-  firstList.sort();
-  for (auto i = firstList.cbegin(); i != firstList.cend(); ++i)
-  {
-    std::cout << (*i) << ' ';
-  }
-  std::cout << '\n';
-
-  secondList.sort();
-  for (auto i = secondList.cbegin(); i != secondList.cend(); ++i)
-  {
-    std::cout << (*i) << ' ';
-  }
-  std::cout << '\n';
-
-  firstBiList.sort(std::greater< int >());
-  for (auto i = firstBiList.cbegin(); i != firstBiList.cend(); ++i)
-  {
-    std::cout << (*i) << ' ';
-  }
-  std::cout << '\n';
-
-  secondBiList.sort(std::greater< int >());
-  for (auto i = secondBiList.cbegin(); i != secondBiList.cend(); ++i)
-  {
-    std::cout << (*i) << ' ';
-  }
-  std::cout << '\n';
-
-  std::sort(firstDeque.begin(), firstDeque.end());
-  for (auto i = firstDeque.cbegin(); i != firstDeque.cend(); ++i)
-  {
-    std::cout << (*i) << ' ';
-  }
-  std::cout << '\n';
-
-  std::sort(secondDeque.begin(), secondDeque.end());
-  for (auto i = secondDeque.cbegin(); i != secondDeque.cend(); ++i)
-  {
-    std::cout << (*i) << ' ';
-  }
-  std::cout << '\n';
 
   return 0;
 }
